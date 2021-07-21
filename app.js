4906a49818919e883a1a43ef8a09bc34d43b2f63
Puranam Karthik Sai Radha Krishna <pkarthiksaai@gmail.com>
2021-07-21 13:25:39 +0000

    added new routesa

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // Routes
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.get("/ping", function (req, res) {
+  res.send("Pong");
+});
+
+app.get("/ping/ping", function (req, res) {
+  res.send("Pong pong");
 });
 
 // Listen
 var port = process.env.PORT || 3000;
 app.listen(port);
-console.log('Listening on localhost:'+ port);
+console.log("Listening on localhost:" + port);
